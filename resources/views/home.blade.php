5552457409ea15d0191b4bb2eb646ee0998c60e1
ramonMyers <ramon_souza@hotmail.com>
2017-07-20 19:17:15 -0300

    Update home.blade.php
    -Aviso de Manutenção

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,7 +67,23 @@
 
 
 @section ('content')
-
+<div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title"><span class="glyphicon glyphicon-wrench"></span> Aviso de Manutenção Programada</h4>
+      </div>
+      <div class="modal-body">
+        <p style='text-align: center'>Informamos que será realizada manutenção no site a partir das
+        <strong>18h</strong> com previsão de término ás <strong>17h</strong></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
 @include('carousel')
 
 <div class="container" style="margin-top:35px;">
@@ -113,6 +129,7 @@
 
 @section('script')
 <script>
+        $('#myModal').modal('show')
 </script>
 @stop
 
